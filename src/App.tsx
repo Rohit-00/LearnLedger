3cc51c17cf77025e17cda6ff203cfac66e80ed8e
Zaid <kzaid2144@gmail.com>
2024-12-28 23:54:43 +0530

    added articles router

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,44 @@
-import { useState } from 'react'
-import './App.css'
-import { ConnectWallet } from './components/connectWallet'
-import Navbar from './components/navbar'
-import  QuizTab  from './pages/quizzes'
-import { QuizPage } from './pages/quiz'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import Layout from './layout'
+import { useState } from "react";
+import "./App.css";
+import { ConnectWallet } from "./components/connectWallet";
+import Navbar from "./components/navbar";
+import QuizTab from "./pages/quizzes";
+import { QuizPage } from "./pages/quiz";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Layout from "./layout";
+import ArticlesTab from "./Pages/articles";
 
 const router = createBrowserRouter([
   {
-    path : '/',
-    element : <Layout/>,
+    path: "/",
+    element: <Layout />,
     children: [
       {
-        path : "/quiz",
-        element : <QuizPage/>
+        path: "/quiz",
+        element: <QuizPage />,
       },
-    
+
       {
-        path : "/quizzes",
-        element : <QuizTab/>
+        path: "/quizzes",
+        element: <QuizTab />,
       },
 
-
-    ]
-
+      {
+        path: "/articles",
+        element: <ArticlesTab />,
+      },
+    ],
   },
-
-])
-
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
